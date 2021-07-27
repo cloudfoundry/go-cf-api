b533b946d63f8c8a5dcf4dfd1536619a0e47450b
Andy Paine <andrew.paine@sap.com>
2021-07-27 17:30:13 +0100

    Split DB initializers more nicely
    We can't actually split these by build tag since Mage wants to use this
    to create connections and it works by compiling a binary (for which it
    would need to know the tag that it is compiling before running).

--- internal/app/cloudgontroller/storage/db/db.go
+++ internal/app/cloudgontroller/storage/db/db.go
@@ -3,17 +3,10 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"net"
-	"strings"
 	"sync"
-	"time"
 
-	"github.com/go-sql-driver/mysql"
-	"github.com/jackc/pgx/v4"
-	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.tools.sap/cloudfoundry/cloudgontroller/internal/app/cloudgontroller/config"
 	"github.tools.sap/cloudfoundry/cloudgontroller/internal/app/cloudgontroller/helpers"
-	"go.uber.org/zap"
 )
 
 var once sync.Once
@@ -30,92 +23,14 @@ type DBInfo struct {
 }
 
 func NewConnection(dbConfig config.DBConfig, connectDB bool) {
-	once.Do(func() {
-		if dbConfig.Type == "postgres" {
-			// Parse Infos from Connection String
-			cfg, err := pgx.ParseConfig(dbConfig.ConnectionString)
-			helpers.CheckErrFatal(err)
-			databaseInfo.Host = cfg.Host
-			databaseInfo.Port = fmt.Sprint(cfg.Port)
-			databaseInfo.DatabaseName = cfg.Database
-			databaseInfo.User = cfg.User
-			databaseInfo.Password = cfg.Password
-			databaseInfo.Type = dbConfig.Type
-
-			// Dont Connect to DB
-			if !connectDB {
-				dbConfig.ConnectionString = strings.ReplaceAll(dbConfig.ConnectionString, fmt.Sprintf("dbname=%s", databaseInfo.DatabaseName), "")
-			}
-
-			zap.L().Debug(fmt.Sprintf("Using DB Connection String: %v",
-				helpers.Redact(dbConfig.ConnectionString, []string{databaseInfo.Password})))
-			database, err = sql.Open("pgx", dbConfig.ConnectionString)
-			helpers.CheckErrFatal(err)
-			// If DB is Missing, we Create it
-			err = database.Ping()
-			if dbConfig.Create && connectDB && err != nil && strings.Contains(err.Error(), fmt.Sprintf("database \"%s\" does not exist (SQLSTATE 3D000)", databaseInfo.DatabaseName)) {
-				zap.L().Info(fmt.Sprintf("Database %s does not exist. Trying to create it", databaseInfo.DatabaseName))
-				database.Close()
-				dblessConnectionString := strings.ReplaceAll(dbConfig.ConnectionString, fmt.Sprintf("dbname=%s", databaseInfo.DatabaseName), "")
-				database, err = sql.Open("pgx", dblessConnectionString)
-				helpers.CheckErrFatal(err)
-				Create(database, databaseInfo)
-				database.Close()
-				database, err = sql.Open("pgx", dbConfig.ConnectionString)
-				helpers.CheckErrFatal(err)
-			} else {
-				helpers.CheckErrFatal(err)
-			}
-		} else {
-			// Parse Infos from Connection String
-			cfg, err := mysql.ParseDSN(dbConfig.ConnectionString)
-			helpers.CheckErrFatal(err)
-			databaseInfo.Host, databaseInfo.Port, err = net.SplitHostPort(cfg.Addr)
-			helpers.CheckErrFatal(err)
-			databaseInfo.DatabaseName = cfg.DBName
-			databaseInfo.User = cfg.User
-			databaseInfo.Password = cfg.Passwd
-			databaseInfo.Type = dbConfig.Type
-
-			if !connectDB {
-				cfg.DBName = ""
-				dbConfig.ConnectionString = cfg.FormatDSN()
-			}
-
-			zap.L().Debug(fmt.Sprintf("Using DB Connection String: %v",
-				helpers.Redact(dbConfig.ConnectionString, []string{databaseInfo.Password})))
-			database, err = sql.Open("mysql", dbConfig.ConnectionString)
-			helpers.CheckErrFatal(err)
-			// If DB is Missing, we Create it
-			err = database.Ping()
-			if err != nil {
-				fmt.Print(err.Error())
-			}
-			if dbConfig.Create && connectDB && err != nil && strings.Contains(err.Error(), fmt.Sprintf("Error 1049: Unknown database '%s'", databaseInfo.DatabaseName)) {
-				zap.L().Info(fmt.Sprintf("Database %s does not exist. Trying to create it", databaseInfo.DatabaseName))
-				database.Close()
-				dblessCfg := cfg.Clone()
-				dblessCfg.DBName = ""
-				dblessCfg.FormatDSN()
-				database, err = sql.Open("mysql", dblessCfg.FormatDSN())
-				helpers.CheckErrFatal(err)
-				Create(database, databaseInfo)
-				database.Close()
-				database, err = sql.Open("mysql", dbConfig.ConnectionString)
-				helpers.CheckErrFatal(err)
-			} else {
-				helpers.CheckErrFatal(err)
-			}
-		}
-
-		// DB Connection Settings
-		database.SetMaxIdleConns(5)
-		database.SetMaxOpenConns(20)
-		database.SetConnMaxLifetime(time.Hour)
-
-		// Check Connection on StartUp
-		helpers.CheckErrFatal(database.Ping())
-	})
+	switch dbConfig.Type {
+	case "postgres":
+		NewPostgresConnection(dbConfig, connectDB)
+	case "mysql":
+		NewMySQLConnection(dbConfig, connectDB)
+	default:
+		helpers.CheckErrFatal(fmt.Errorf("unrecognized database type %s", dbConfig.Type))
+	}
 }
 
 func GetConnection() *sql.DB {
---
+++ internal/app/cloudgontroller/storage/db/postgres.go
@@ -0,0 +1,64 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.tools.sap/cloudfoundry/cloudgontroller/internal/app/cloudgontroller/config"
+	"github.tools.sap/cloudfoundry/cloudgontroller/internal/app/cloudgontroller/helpers"
+	"go.uber.org/zap"
+)
+
+func NewPostgresConnection(dbConfig config.DBConfig, connectDB bool) {
+	if dbConfig.Type != "postgres" {
+		helpers.CheckErrFatal(fmt.Errorf("cannot use database of type %s with postgres binary", dbConfig.Type))
+	}
+	once.Do(func() {
+		// Parse Infos from Connection String
+		cfg, err := pgx.ParseConfig(dbConfig.ConnectionString)
+		helpers.CheckErrFatal(err)
+		databaseInfo.Host = cfg.Host
+		databaseInfo.Port = fmt.Sprint(cfg.Port)
+		databaseInfo.DatabaseName = cfg.Database
+		databaseInfo.User = cfg.User
+		databaseInfo.Password = cfg.Password
+		databaseInfo.Type = dbConfig.Type
+
+		// Dont Connect to DB
+		if !connectDB {
+			dbConfig.ConnectionString = strings.ReplaceAll(dbConfig.ConnectionString, fmt.Sprintf("dbname=%s", databaseInfo.DatabaseName), "")
+		}
+
+		zap.L().Debug(fmt.Sprintf("Using DB Connection String: %v",
+			helpers.Redact(dbConfig.ConnectionString, []string{databaseInfo.Password})))
+		database, err = sql.Open("pgx", dbConfig.ConnectionString)
+		helpers.CheckErrFatal(err)
+		// If DB is Missing, we Create it
+		err = database.Ping()
+		if dbConfig.Create && connectDB && err != nil && strings.Contains(err.Error(), fmt.Sprintf("database \"%s\" does not exist (SQLSTATE 3D000)", databaseInfo.DatabaseName)) {
+			zap.L().Info(fmt.Sprintf("Database %s does not exist. Trying to create it", databaseInfo.DatabaseName))
+			database.Close()
+			dblessConnectionString := strings.ReplaceAll(dbConfig.ConnectionString, fmt.Sprintf("dbname=%s", databaseInfo.DatabaseName), "")
+			database, err = sql.Open("pgx", dblessConnectionString)
+			helpers.CheckErrFatal(err)
+			Create(database, databaseInfo)
+			database.Close()
+			database, err = sql.Open("pgx", dbConfig.ConnectionString)
+			helpers.CheckErrFatal(err)
+		} else {
+			helpers.CheckErrFatal(err)
+		}
+
+		// DB Connection Settings
+		database.SetMaxIdleConns(5)
+		database.SetMaxOpenConns(20)
+		database.SetConnMaxLifetime(time.Hour)
+
+		// Check Connection on StartUp
+		helpers.CheckErrFatal(database.Ping())
+	})
+}
