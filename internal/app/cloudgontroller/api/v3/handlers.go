3035671791482e509ac1a07ceff4a41151ef09a0
I505418 <marc.misoch@sap.com>
2021-08-04 15:42:10 +0200

    CFP-1721 Require authentication for /buildpack endpoints

--- internal/app/cloudgontroller/api/v3/handlers.go
+++ internal/app/cloudgontroller/api/v3/handlers.go
@@ -2,6 +2,8 @@ package v3
 
 import (
 	"fmt"
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -16,9 +18,30 @@ func RegisterHealthHandler(e *echo.Echo) {
 }
 
 func RegisterV3Handlers(prefix string, e *echo.Echo) {
-	// Buildpacks
-	e.GET(fmt.Sprintf("%s/buildpacks", prefix), controllers.GetBuildpacks)
-	e.GET(fmt.Sprintf("%s/buildpacks/:guid", prefix), controllers.GetBuildpack)
+	var uaakey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtDbj14vkov7AFq6fVEJN
++1qqwfgGDIj7k3sohtwbWvTHQA3kWXb6ffUyVtUrYWk5LcQLT2v4fEzNInCif082
+yGwKdtTRG8kXDEyXT1CyydK+LznneH+AOxno3I8QKxm3Vhl1pcV0ZR9p928FdsHn
+wFDy2hnPFLmEnKn5HEU+zzE39NgO/h8ZJvcA2sO+PgtJd8YY2gXWiHzzrbCH9+Kd
+rJprXeFqA8WIRWvBYckzXZFQtwqk9blX0CndgNGog7dwDmoLYdHa6e5p5tSuqhAp
+QRonhJgGhX1Kj94TB+msTsucDExLPZMTx/OKNVyTFwsOmTrQPl0Fb1pXMf5S0d7E
+tQIDAQAB
+-----END PUBLIC KEY-----
+`
+	var publicKey, _ = jwt.ParseRSAPublicKeyFromPEM([]byte(uaakey))
+	// Restricted group
+	r := e.Group(prefix)
+	{
+		config := middleware.JWTConfig{
+			SigningKey: publicKey,
+			SigningMethod: "RS256",
+		}
+		r.Use(middleware.JWTWithConfig(config))
+
+		// Buildpacks
+		r.GET(fmt.Sprintf("/buildpacks"), controllers.GetBuildpacks)
+		r.GET(fmt.Sprintf("/buildpacks/:guid"), controllers.GetBuildpack)
+	}
 }
 
 func RegisterV3DocumentationHandlers(prefix string, e *echo.Echo) {
