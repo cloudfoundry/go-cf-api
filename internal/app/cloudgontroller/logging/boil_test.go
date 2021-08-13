993d418a1bebae7cd623f0893b0302cc69ec80c0
I546215 <a.alam@sap.com>
2021-08-13 14:22:48 +0200

    Add improved test cases for boil_test

--- internal/app/cloudgontroller/logging/boil_test.go
+++ internal/app/cloudgontroller/logging/boil_test.go
@@ -13,29 +13,52 @@ import (
 	"go.uber.org/zap/zaptest/observer"
 )
 
-func TestBoilLogger(t *testing.T) {
+func TestBoilerForBothCases(t *testing.T) {
 	t.Parallel()
+	// with parameters redacted
+	testBoilLoggerParams(t, true)
+	// without parameters redacted
+	testBoilLoggerParams(t, false)
+}
 
+func testBoilLoggerParams(t *testing.T, redactParams bool) {
+	t.Helper()
 	obs, logs := observer.New(zap.DebugLevel)
+	assertEmpty(t, logs)
 	assert.NoError(t, obs.Sync(), "Unexpected failure in no-op Sync")
-
 	logger := zap.New(obs)
-	bl := logging.NewBoilLogger(true, logger)
-	_, err := bl.Write([]byte("SELECT * FROM \"buildpacks\" LIMIT 50;"))
+
+	bl := logging.NewBoilLogger(redactParams, logger)
+	_, err := bl.Write([]byte("SELECT 1 from where you would like"))
 	assert.NoError(t, err)
 	_, err = bl.Write([]byte(""))
 	assert.NoError(t, err)
 
+	params := ""
+	if redactParams {
+		params = "REDACTED"
+	}
 	want := observer.LoggedEntry{
 		Entry: zapcore.Entry{Level: zap.DebugLevel, Message: "SQL LOG"},
 		Context: []zapcore.Field{
-			zap.String("query", "SELECT * FROM \"buildpacks\" LIMIT 50;"),
-			zap.String("params", "REDACTED"),
+			zap.String("query", "SELECT 1 from where you would like"),
+			zap.String("params", params),
 		},
 	}
 	assert.Len(t, logs.AllUntimed(), 1)
 	log := logs.AllUntimed()[0]
+	assertEqual(t, want, log)
+}
+
+func assertEqual(t *testing.T, want, log observer.LoggedEntry) {
+	t.Helper()
 	assert.Equal(t, want.Context, log.Context)
 	assert.Equal(t, want.Entry.Level, log.Level)
 	assert.Equal(t, want.Entry.Message, log.Message)
+}
+
+//nolint
+func assertEmpty(t testing.TB, logs *observer.ObservedLogs) {
+	assert.Equal(t, 0, logs.Len(), "Expected empty ObservedLogs to have zero length.")
+	assert.Equal(t, []observer.LoggedEntry{}, logs.All(), "Unexpected LoggedEntries in empty ObservedLogs.")
 }
