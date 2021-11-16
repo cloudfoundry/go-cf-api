386676fa8cbf0ffc36965493ca52c4bcd4ccb9b5
Andy Paine <andrew.paine@sap.com>
2021-11-16 09:09:11 +0000

    Add more test cases to timefilter parser
    Make sure error cases etc. are handled properly

--- internal/app/cloudgontroller/api/v3/timefilters/parser_test.go
+++ internal/app/cloudgontroller/api/v3/timefilters/parser_test.go
@@ -23,20 +23,35 @@ func TestParseTimeFilters(t *testing.T) {
 		expectedUpdatedAts []TimeFilter
 		expectedError      error
 	}{
-		"filter by time": {
-			query: fmt.Sprintf("created_ats=%s&updated_ats=%s", now, now),
+		"equal": {
+			query:              fmt.Sprintf("created_ats=%s&updated_ats=%s", now, now),
+			expectedCreatedAts: []TimeFilter{{Operator: "", Timestamp: nowT}},
+			expectedUpdatedAts: []TimeFilter{{Operator: "", Timestamp: nowT}},
+		},
+		"less than": {
+			query:              fmt.Sprintf("created_ats[lt]=%s&updated_ats[lte]=%s", now, now),
+			expectedCreatedAts: []TimeFilter{{Operator: "[lt]", Timestamp: nowT}},
+			expectedUpdatedAts: []TimeFilter{{Operator: "[lte]", Timestamp: nowT}},
+		},
+		"greater than": {
+			query:              fmt.Sprintf("created_ats[gt]=%s&updated_ats[gte]=%s", now, now),
+			expectedCreatedAts: []TimeFilter{{Operator: "[gt]", Timestamp: nowT}},
+			expectedUpdatedAts: []TimeFilter{{Operator: "[gte]", Timestamp: nowT}},
+		},
+		"repeated field with different comparisons": {
+			query: fmt.Sprintf("created_ats[gt]=%s&created_ats[lte]=%s", now, now),
 			expectedCreatedAts: []TimeFilter{
-				{
-					Operator:  "",
-					Timestamp: nowT,
-				},
+				{Operator: "[gt]", Timestamp: nowT},
+				{Operator: "[lte]", Timestamp: nowT},
 			},
-			expectedUpdatedAts: []TimeFilter{
-				{
-					Operator:  "",
-					Timestamp: nowT,
-				},
-			},
+		},
+		"unrecognized comparison": {
+			query:         fmt.Sprintf("created_ats[foo]=%s", now),
+			expectedError: fmt.Errorf("unrecognized comparison operator '[foo]'"),
+		},
+		"malformed filter": {
+			query:         fmt.Sprintf("created_ats[gte=%s", now),
+			expectedError: fmt.Errorf("could not parse 'created_ats' filter created_ats[gte"),
 		},
 	}
 
